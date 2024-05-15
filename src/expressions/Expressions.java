d6290f9bb3030c5f47396e4438b717a9d21ce52a
soumyaathanimath <>
2024-05-15 15:27:04 +0530

    regular expressions

---
+++ src/expressions/Expressions.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package expressions;
+
+/**
+ *
+ * @author dell1
+ */
+public class Expressions {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        /*String str1="f";
+        System.out.println(str1.matches("."));*/
+        
+        /*String str1="a";//b,c
+        System.out.println(str1.matches("[abc]"));*/
+        
+        /*String str1="p";//true for alphabets other than abc
+        System.out.println(str1.matches("[^abc]"));*/
+        
+        /*String str1="7";//A,a7
+        System.out.println(str1.matches("[a-zA-Z0-9]"));//[a-z][0-9]*/
+        
+        /*String str1="b";
+        System.out.println(str1.matches("a|b"));*/
+        
+        /*String str1="b";
+        System.out.println(str1.matches("\\w"));*/
+        
+        /*String str1="5";
+        System.out.println(str1.matches("\\d"));*/
+        
+        String str1="$";
+        System.out.println(str1.matches("\\D"));
+    }
+    
+}
+ 
